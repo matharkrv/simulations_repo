4ac9289041e668a0bb8a26057af23afbf40daed3
matharkrv <matark@mail.tau.ac.il>
2024-11-26 12:05:37 +0200

    Update new_simulation.py

--- new_simulation.py
+++ new_simulation.py
@@ -6,18 +6,18 @@ from numba import prange
 from numpy import cos, sin
 from numpy.random import SeedSequence
 from tqdm import tqdm
-
+from matplotlib.colors import LogNorm
+import matplotlib.cm as cm
 import form_factor_methods as form_factor
 from istarmap import istarmap
 from simulation_methods import *
-from matplotlib.colors import LogNorm
-import matplotlib.cm as cm
+from typing import List, Callable, Optional, Tuple
 
 # Import istarmap for simultaneous tracking of the multiprocess and its execution
 mpp.Pool.istarmap = istarmap
 
 
-def simulation_process_2D(*args):
+def _simulation_process_2D(*args):
     """
         Perform the 2D simulation process for X-ray scattering.
 
@@ -57,25 +57,18 @@ def simulation_process_2D(*args):
         delta_q,
         inv_cdf_interp,
         sample_detector_distance, k, pixel_number, centers, px_size) = args
-    # Generate a randomization using the given seed 
     rng = np.random.default_rng(seed)
-    # Initialize the intensity update array
     intensity_update = np.zeros([pixel_number, pixel_number]).astype(np.float64)
     for _ in prange(batch):
-        # Sample a random exit angle q from the inverse cdf
         q_random = inv_cdf_interp(rng.uniform(0, 1, size=sampling_size))
         if np.any(np.isfinite(q_random)):
-            # Find the angle
             angle_delta = np.arcsin(q_random / (2 * k))
-            # Generate a random azimuth angle from 0 to 2pi
             azimuth_angle = rng.uniform(0, 2 * np.pi - np.finfo(np.float64).eps, size=sampling_size)
         else:
             angle_delta = 0
             azimuth_angle = 0
-        # Generate a random set of entry angles and slit positions
         theta_in, chi_in, sl2_x, sl2_y = sample_from_slits(rng, slit_1_x, slit_1_y, slit_2_x, slit_2_y, slit_distance,
                                                            sampling_size)
-        # Preliminary calculations of the sin and cos of all the angles
         sin_T = sin(angle_delta)
         cos_T = cos(angle_delta)
         sin_t = sin(theta_in)
@@ -84,7 +77,6 @@ def simulation_process_2D(*args):
         cos_chi = cos(chi_in)
         sin_Chi = sin(azimuth_angle)
         cos_Chi = cos(azimuth_angle)
-        # Perform the equations to find the incident positions on the detector (x, y)
         r_x = sin_T * (cos_chi * cos_Chi * cos_t - sin_chi * sin_Chi) + cos_chi * sin_t * cos_T
         r_y = sin_T * (sin_chi * cos_Chi * cos_t + cos_chi * sin_Chi) + sin_chi * sin_t * cos_T
         r_z = cos_Chi * sin_t * sin_T - cos_t * cos_T
@@ -92,7 +84,6 @@ def simulation_process_2D(*args):
         y1 = sample_detector_distance * (r_y / r_z) + sl2_y
         x = x1 / px_size + centers[0]
         y = y1 / px_size + centers[1]
-        # Mask out every incident position outside of the detector 
         mask = (x <= pixel_number - 1) & (y <= pixel_number - 1) & (x >= 0) & (y >= 0)
         y_masked = np.round(y[mask], 0).astype(int)
         x_masked = np.round(x[mask], 0).astype(int)
@@ -103,9 +94,21 @@ def simulation_process_2D(*args):
     return intensity_update
 
 
-def IqMC_2D(wavelength, slit1_radius, slit2_radius, sample_detector_distance, slit_distance, detector_length,
-            detector_center, pixel_number, photon_number, LUT_resolution, sampling_size,
-            scattering_function, Rg_array, distribution, q_max=None):
+def IqMC_2D(wavelength: float,
+            slit1_radius: List[float],
+            slit2_radius: List[float],
+            sample_detector_distance: float,
+            slit_distance: float,
+            detector_length: float,
+            detector_center: List[float],
+            pixel_number: int,
+            photon_number: int,
+            LUT_resolution: int,
+            sampling_size: int,
+            scattering_function: Callable,
+            Rg_array: np.ndarray,
+            distribution: np.ndarray,
+            q_max: Optional[float] = None) -> Tuple[np.ndarray, np.ndarray]:
     """
     Monte Carlo simulation for X-ray scattering intensity profile calculation.
 
@@ -123,7 +126,7 @@ def IqMC_2D(wavelength, slit1_radius, slit2_radius, sample_detector_distance, sl
     sampling_size (int): Amount of scattering incidents to be generated at once at each simulation run.
     scattering_function (function): Scattering form factor function.
     Rg_array (array): Array of all possible Rgs.
-    Distribution (array): Distribution for the array of Rgs.
+    distribution (array): Distribution for the array of Rgs.
     q_max (float, optional): Maximum q value. If not provided, it is calculated based on the given parameters.
 
     Returns:
@@ -166,7 +169,7 @@ def IqMC_2D(wavelength, slit1_radius, slit2_radius, sample_detector_distance, sl
              sample_detector_distance, k, pixel_number, pixel_centers, px_size) for i in range(batch_number)]
     with mp.Pool(processes=num_workers) as pool:
         results = []
-        for result in tqdm(pool.istarmap(simulation_process_2D, args), total=batch_number):
+        for result in tqdm(pool.istarmap(_simulation_process_2D, args), total=batch_number):
             results.append(result)
     for result in results:
         Intensity_profile += result.astype(float)
@@ -175,11 +178,33 @@ def IqMC_2D(wavelength, slit1_radius, slit2_radius, sample_detector_distance, sl
     return Intensity_profile, q_out_array
 
 
-if __name__ == "__main__":
+def example_simulation():
+    """
+    This function performs an example Monte Carlo simulation for a "Guinier form factor".
+
+    Parameters:
+    wavelength_ (float): X-ray wavelength in centimeters.
+    slit1_radius_ (list): Half-length of slit 1 in centimeters.
+    slit2_radius_ (list): Half-length of slit 2 in centimeters.
+    sample_detector_distance_ (float): Distance between the sample and the detector.
+    slit_distance_ (float): Distance between the slit.
+    detector_length_ (float): Length of the detector in centimeters.
+    detector_center_ (list): Center of the detector in centimeters.
+    pixel_number_ (int): Number of pixels on the detector.
+    photon_number_ (int): Number of photons (simulation runs).
+    LUT_resolution_ (int): Resolution of the initial scattering probability distribution LUT.
+    sampling_size_ (int): Amount of scattering incidents to be generated at once at each simulation run.
+    Rg_average_ (float): Average radius of gyration, in centimeters.
+    Rg_variance_ (float): Relative variance of the radius of gyration, unitless.
+
+    Returns:
+    tuple: A tuple containing the final Intensity profile and the array of exit wave vectors.
+    """
+
     wavelength_ = 1.54e-8  # X-ray wavelength in centimeters
     slit1_radius_ = [0.05, 0.05]  # Half-length of slit 1 in centimeters
     slit2_radius_ = [0.03, 0.03]  # Half-length of slit 2 in centimeters
-    sample_detector_distance_ = 200  # Distance between the sample and the detector
+    sample_detector_distance_ = 100  # Distance between the sample and the detector
     slit_distance_ = 200  # Distance between the slit
     detector_length_ = 7.5  # Length of the detector in centimeters
     detector_center_ = [3, 3]  # Center of the detector in centimeters
@@ -188,7 +213,7 @@ if __name__ == "__main__":
     LUT_resolution_ = int(1e4)  # Resolution of the initial scattering probability distribution LUT
     sampling_size_ = int(1e4)  # Amount of scattering incidents to be generated at once at each simulation run
     Rg_average_ = 4e-7  # Average radius of gyration, in centimeters
-    Rg_variance_ = 0.0001  # Relative variance of the radius of gyration, unitless
+    Rg_variance_ = 0.05  # Relative variance of the radius of gyration, unitless
 
     # Generate the distribution of Rgs
     rg_array, distribution_ = form_factor.generate_gaussian_distribution(Rg_average_,
@@ -203,10 +228,10 @@ if __name__ == "__main__":
                              sampling_size_, form_factor.guinier_ff, rg_array, distribution_, q_max=q_max_)
 
     # Save the intensity profile and the q-table as TIF files
-    tif.imwrite(output_path + f"dist200_intensity.tif",
+    tif.imwrite(output_path + f"dist100_intensity.tif",
                 det_2d, photometric='minisblack')
     # Here the q values are normalized by 1e-7 to change their units to nm^-1.
-    tif.imwrite(output_path + f"dist200_qtable.tif",
+    tif.imwrite(output_path + f"dist100_qtable.tif",
                 qtable * 1e-7, photometric='minisblack')
     # Flatten the intensity profile of the scattering. Here the Q values are normalized to be in nm^-1 (multiplication
     # by 1e-7).
@@ -217,7 +242,7 @@ if __name__ == "__main__":
     px_size_ = ((detector_length_ * 1e4) / pixel_number_) / 100
     poni1 = detector_center_[0] / 100  # meters
     poni2 = detector_center_[1] / 100  # meters
-    with open(output_path + f"poni200.poni", 'w') as file:
+    with open(output_path + f"poni100.poni", 'w') as file:
         file.write("Detector: Simulation")
         file.write(f"Detector_config: pixel1: {px_size_}, pixel2: {px_size_}, max_shape: [{pixel_number_},"
                    f" {pixel_number_}]")
@@ -231,10 +256,9 @@ if __name__ == "__main__":
         file.write(f"wavelength: {wavelength_ / 100}")
 
     # Example for plotting the 1D data
-    plt.figure(1)
     q_range = (xq >= 0.05) & (xq <= 1)
     plt.scatter(xq[q_range], yi[q_range], color='cyan', s=5, label='simulated', zorder=1)
-    plt.plot(xq[q_range], yi[q_range][0]*form_factor.guinier_ff(xq[q_range], 4),
+    plt.plot(xq[q_range], yi[q_range][0] * form_factor.guinier_ff(xq[q_range], 4),
              color='black', ls='dashed', lw=2, label='form factor', zorder=3)
     plt.xlabel('q (nm^-1)')
     plt.ylabel('Intensity (arb. units)')
@@ -242,9 +266,8 @@ if __name__ == "__main__":
     plt.legend(frameon=False)
     plt.yscale('log')
     plt.xlim(xq[q_range][0], xq[q_range][-1])
-
+    plt.show()
     #Plotting the 2D data
-    #plt.imshow(det_2d)
     f = plt.figure(figsize=(6.2, 5.6))
     ax = f.add_axes([0.17, 0.02, 0.72, 0.79])
     axcolor = f.add_axes([0.90, 0.02, 0.03, 0.79])
@@ -253,3 +276,4 @@ if __name__ == "__main__":
     f.colorbar(im, cax=axcolor, format="$%.2f$")
     f.show()
     plt.show()
+    return det_2d, qtable
